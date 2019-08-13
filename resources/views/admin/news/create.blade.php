593e501886004b0c2d8c7661bd80643962dc0ade
”名前” <uryuisamu@gmail.com>
2019-08-13 08:27:40 +0000

    No21

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,23 +1,15 @@
+@extends('layouts.admin')
 
-   
-       
-        @extends('layouts.admin')
-        <title>MyNews</title>
-        
-        @section('title', 'ニュースの新規作成')
-        @section('content')
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 mx-auto">
-                        <h2>ニュース新規作成</h2>
-                    </div>
-                </div>
+@section('title', 'ニュースの新規作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>ニュース新規作成</h2>
             </div>
-        @endsection    
-   
-    <body>
-        <h1>Myニュース作成画面</h1>
-        @foreach($newsList as $news)  
-           {{$news}}<br>
-        @endforeach
-    </body>
+        </div>
+    </div>
+    @foreach($newsList as $news)  
+       {{$news}}<br>
+    @endforeach
+@endsection
