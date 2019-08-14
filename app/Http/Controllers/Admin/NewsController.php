998ba95a1724d1f2d211ec46f15e2941669fe474
”名前” <uryuisamu@gmail.com>
2019-08-14 12:46:28 +0000

    No22

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -9,16 +9,12 @@ class NewsController extends Controller
 {
     //
     public function add(){
-        //var_dump("ts");
-        //$a = 22222;
-        //$b = "test";
-        //$c = [$a, $b];
-        //var_dump($a, $b, $c);
-        //var_dump();
-        //echo $a;
-        //error_log($a);
-        $newsList = ["よしもと炎上", "選挙", "スマホ"];
-        return view('admin.news.create', compact("newsList"));
+        
+        return view('admin.news.create');
+    } 
     
-    }    
+    public function create(Request $request){
+      
+      return redirect('admin/news/create');
+    }
 }
