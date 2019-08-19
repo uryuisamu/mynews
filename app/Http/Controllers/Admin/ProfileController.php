7d595cd46a618784007d326d7a967de7c2787d98
”名前” <uryuisamu@gmail.com>
2019-08-19 11:05:33 +0000

    No22&23

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,23 +1,27 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profiles;
+
 class ProfileController extends Controller 
 {
-    //課題3・4　　
-    //以下にあるadd Action と edit Action を描画するには、
-    //それぞれどこのディレクトリに何というbladeファイルを設置すれば良いでしょうか。
-    //※ファイルの名前（検索）・・・addのblade：create.blade.php   editのblade:edit.blade.php
-    
-    
     public function add(){
         return view('admin.profile.create');
     }
     
-    public function create(){
+    public function create(Request $request){
+        $this->validate($request, Profiles::$rules);
+        $profiles = new Profiles;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $profiles->fill($form);
+        $profiles->save();
+        
         return redirect('admin/profile/create');
     }
     
